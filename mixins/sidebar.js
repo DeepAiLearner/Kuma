eec3d0698f72ab1c2f3d306d7b45d2853a3908db
falstack <icesilt@outlook.com>
2018-11-28 21:40:38 +0800

    fixed: icon

--- mixins/sidebar.js
+++ mixins/sidebar.js
@@ -2,19 +2,13 @@ export default {
   methods: {
     mapTitle(title) {
       return {
-        dashboard: '首页',
-        bangumi: '番剧',
-        'bangumi-list': '番剧列表',
-        'bangumi-manager': '设置版主'
+        dashboard: '首页'
       }[title]
     },
     mapIcon(icon) {
       // icons: https://fontawesome.com/icons?d=gallery&s=solid
       return {
-        dashboard: 'tachometer-alt',
-        bangumi: 'globe',
-        'bangumi-list': 'th-list',
-        'bangumi-manager': 'user-cog'
+        dashboard: 'tachometer-alt'
       }[icon]
     }
   }
